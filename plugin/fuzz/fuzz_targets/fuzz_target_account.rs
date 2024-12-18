1463f015c109a42243b05e278daf84c841fb8595
lyranowl <nikita.malina23@gmail.com>
2024-12-18 21:58:54 +0500

    implemented `FuzzSanitizedTransaction` in `fuzz_target_transaction`

--- plugin/fuzz/fuzz_targets/fuzz_target_account.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_account.rs
@@ -4,13 +4,11 @@ use agave_geyser_plugin_interface::geyser_plugin_interface::ReplicaAccountInfoV3
 use libfuzzer_sys::fuzz_target;
 use richat_plugin::protobuf::ProtobufMessage;
 
-const PUBKEY_LEN: usize = 32;
-
 #[derive(arbitrary::Arbitrary, Debug)]
 pub struct Account<'a> {
-    pubkey: &'a [u8],
+    pubkey: [u8; 32],
     lamports: u64,
-    owner: &'a [u8],
+    owner: [u8; 32],
     executable: bool,
     rent_epoch: u64,
     data: &'a [u8],
@@ -18,25 +16,19 @@ pub struct Account<'a> {
 }
 
 #[derive(arbitrary::Arbitrary, Debug)]
-pub struct FuzzMessage<'a> {
+pub struct FuzzAccountMessage<'a> {
     slot: u64,
     account: Account<'a>,
 }
 
-fuzz_target!(|fuzz_message: FuzzMessage| {
-    if fuzz_message.account.pubkey.len() != PUBKEY_LEN
-        || fuzz_message.account.owner.len() != PUBKEY_LEN
-    {
-        return;
-    }
-
+fuzz_target!(|fuzz_message: FuzzAccountMessage| {
     let mut buf = Vec::new();
     let message = ProtobufMessage::Account {
         slot: fuzz_message.slot,
         account: &ReplicaAccountInfoV3 {
-            pubkey: fuzz_message.account.pubkey,
+            pubkey: &fuzz_message.account.pubkey,
             lamports: fuzz_message.account.lamports,
-            owner: fuzz_message.account.owner,
+            owner: &fuzz_message.account.owner,
             executable: fuzz_message.account.executable,
             rent_epoch: fuzz_message.account.rent_epoch,
             data: fuzz_message.account.data,
--- plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
@@ -1,7 +1,118 @@
+#![allow(dead_code)] // FIXME: remove it!!
 #![no_main]
 
+use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-});
+pub mod sanitazed {
+    use arbitrary::Arbitrary;
+    use std::borrow::Cow;
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzLegacyMessageInner {
+        header: FuzzMessageHeader,
+        account_keys: Vec<[u8; 32]>,
+        recent_blockhash: [u8; 32],
+        instructions: Vec<FuzzCompiledInstruction>,
+    }
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzMessageAddressTableLookup {
+        account_key: [u8; 32],
+        writable_indexes: Vec<u8>,
+        readonly_indexes: Vec<u8>,
+    }
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzMessageHeader {
+        num_required_signatures: u8,
+        num_readonly_signed_accounts: u8,
+        num_readonly_unsigned_accounts: u8,
+    }
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzCompiledInstruction {
+        program_id_index: u8,
+        accounts: Vec<u8>,
+        data: Vec<u8>,
+    }
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzLoadedMessageInner {
+        header: FuzzMessageHeader,
+        account_keys: Vec<[u8; 32]>,
+        recent_blockhash: [u8; 32],
+        instructions: Vec<FuzzCompiledInstruction>,
+        address_table_lookups: Vec<FuzzMessageAddressTableLookup>,
+    }
+
+    #[derive(Arbitrary, Debug, Clone)]
+    pub struct FuzzLoadedAddresses {
+        writable: Vec<[u8; 32]>,
+        readonly: Vec<[u8; 32]>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzLegacyMessage<'a> {
+        message: Cow<'a, FuzzLegacyMessageInner>,
+        is_writable_account_cache: Vec<bool>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzLoadedMessage<'a> {
+        message: Cow<'a, FuzzLoadedMessageInner>,
+        loaded_addresses: Cow<'a, FuzzLoadedAddresses>,
+        is_writable_account_cache: Vec<bool>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub enum FuzzSanitizedMessage<'a> {
+        Legacy(FuzzLegacyMessage<'a>),
+        V0(FuzzLoadedMessage<'a>),
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzSanitizedTransaction<'a> {
+        message: FuzzSanitizedMessage<'a>,
+        message_hash: [u8; 32],
+        is_simple_vote_tx: bool,
+        signatures: Vec<&'a [u8]>,
+    }
+}
+
+pub mod status_meta {
+    use arbitrary::Arbitrary;
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzTransactionStatusMeta {
+        status: (), // TODO
+        fee: u64,
+        pre_balances: Vec<u64>,
+        post_balances: Vec<u64>,
+        inner_instructions: Option<Vec<()>>, // TODO
+        log_messages: Option<Vec<String>>,
+        pre_token_balances: Option<Vec<()>>,  // TODO,
+        post_token_balances: Option<Vec<()>>, // TODO
+        rewards: Option<Vec<()>>,             // TODO
+        loaded_addresses: super::sanitazed::FuzzLoadedAddresses,
+        return_data: Option<()>, // TODO
+        compute_units_consumed: u64,
+    }
+}
+
+#[derive(Arbitrary, Debug)]
+pub struct FuzzTransaction<'a> {
+    pub signature: &'a [u8],
+    pub is_vote: bool,
+    pub transaction: sanitazed::FuzzSanitizedTransaction<'a>,
+    pub transaction_status_meta: status_meta::FuzzTransactionStatusMeta,
+    pub index: usize,
+}
+
+#[derive(Arbitrary, Debug)]
+pub struct FuzzTransactionMessage<'a> {
+    slot: u64,
+    transaction: FuzzTransaction<'a>,
+}
+
+fuzz_target!(|fuzz_message: FuzzTransactionMessage| {});
