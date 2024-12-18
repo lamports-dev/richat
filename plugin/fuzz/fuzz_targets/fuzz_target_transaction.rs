cda7f7c348b51becf70786c9dc0067f59f9ec4d5
lyranowl <nikita.malina23@gmail.com>
2024-12-18 22:14:52 +0500

    implemented `FuzzTransactionStatusMeta` in `fuzz_target_transaction`

--- plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
@@ -4,7 +4,20 @@
 use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 
-pub mod sanitazed {
+#[derive(Arbitrary, Debug, Clone)]
+pub struct FuzzCompiledInstruction {
+    program_id_index: u8,
+    accounts: Vec<u8>,
+    data: Vec<u8>,
+}
+
+#[derive(Arbitrary, Debug, Clone)]
+pub struct FuzzLoadedAddresses {
+    writable: Vec<[u8; 32]>,
+    readonly: Vec<[u8; 32]>,
+}
+
+pub mod sanitized {
     use arbitrary::Arbitrary;
     use std::borrow::Cow;
 
@@ -13,7 +26,7 @@ pub mod sanitazed {
         header: FuzzMessageHeader,
         account_keys: Vec<[u8; 32]>,
         recent_blockhash: [u8; 32],
-        instructions: Vec<FuzzCompiledInstruction>,
+        instructions: Vec<super::FuzzCompiledInstruction>,
     }
 
     #[derive(Arbitrary, Debug, Clone)]
@@ -31,25 +44,12 @@ pub mod sanitazed {
     }
 
     #[derive(Arbitrary, Debug, Clone)]
-    pub struct FuzzCompiledInstruction {
-        program_id_index: u8,
-        accounts: Vec<u8>,
-        data: Vec<u8>,
-    }
-
-    #[derive(Arbitrary, Debug, Clone)]
     pub struct FuzzLoadedMessageInner {
         header: FuzzMessageHeader,
         account_keys: Vec<[u8; 32]>,
         recent_blockhash: [u8; 32],
-        instructions: Vec<FuzzCompiledInstruction>,
+        instructions: Vec<super::FuzzCompiledInstruction>,
         address_table_lookups: Vec<FuzzMessageAddressTableLookup>,
-    }
-
-    #[derive(Arbitrary, Debug, Clone)]
-    pub struct FuzzLoadedAddresses {
-        writable: Vec<[u8; 32]>,
-        readonly: Vec<[u8; 32]>,
     }
 
     #[derive(Arbitrary, Debug)]
@@ -61,7 +61,7 @@ pub mod sanitazed {
     #[derive(Arbitrary, Debug)]
     pub struct FuzzLoadedMessage<'a> {
         message: Cow<'a, FuzzLoadedMessageInner>,
-        loaded_addresses: Cow<'a, FuzzLoadedAddresses>,
+        loaded_addresses: Cow<'a, super::FuzzLoadedAddresses>,
         is_writable_account_cache: Vec<bool>,
     }
 
@@ -84,18 +84,75 @@ pub mod status_meta {
     use arbitrary::Arbitrary;
 
     #[derive(Arbitrary, Debug)]
+    pub enum FuzzTransactionError {
+        Err,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzInnerInstruction {
+        instruction: super::FuzzCompiledInstruction,
+        stack_height: Option<u32>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzInnerInstructions {
+        index: u8,
+        instructions: Vec<FuzzInnerInstruction>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzUiTokenAmount {
+        ui_amount: Option<f64>,
+        decimals: u8,
+        amount: String,
+        ui_amount_string: String,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzTransactionTokenBalance {
+        account_index: u8,
+        mint: String,
+        ui_token_amount: FuzzUiTokenAmount,
+        owner: String,
+        program_id: String,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub enum FuzzRewardType {
+        Fee,
+        Rent,
+        Staking,
+        Voting,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzReward {
+        pubkey: String,
+        lamports: i64,
+        post_balance: u64,
+        reward_type: Option<FuzzRewardType>,
+        commission: Option<u8>,
+    }
+
+    #[derive(Arbitrary, Debug)]
+    pub struct FuzzTransactionReturnData {
+        program_id: [u8; 32],
+        data: Vec<u8>,
+    }
+
+    #[derive(Arbitrary, Debug)]
     pub struct FuzzTransactionStatusMeta {
-        status: (), // TODO
+        status: Result<(), FuzzTransactionError>,
         fee: u64,
         pre_balances: Vec<u64>,
         post_balances: Vec<u64>,
-        inner_instructions: Option<Vec<()>>, // TODO
+        inner_instructions: Option<Vec<FuzzInnerInstructions>>,
         log_messages: Option<Vec<String>>,
-        pre_token_balances: Option<Vec<()>>,  // TODO,
-        post_token_balances: Option<Vec<()>>, // TODO
-        rewards: Option<Vec<()>>,             // TODO
-        loaded_addresses: super::sanitazed::FuzzLoadedAddresses,
-        return_data: Option<()>, // TODO
+        pre_token_balances: Option<Vec<FuzzTransactionTokenBalance>>,
+        post_token_balances: Option<Vec<FuzzTransactionTokenBalance>>,
+        rewards: Option<Vec<FuzzReward>>,
+        loaded_addresses: super::FuzzLoadedAddresses,
+        return_data: Option<FuzzTransactionReturnData>,
         compute_units_consumed: u64,
     }
 }
@@ -104,7 +161,7 @@ pub mod status_meta {
 pub struct FuzzTransaction<'a> {
     pub signature: &'a [u8],
     pub is_vote: bool,
-    pub transaction: sanitazed::FuzzSanitizedTransaction<'a>,
+    pub transaction: sanitized::FuzzSanitizedTransaction<'a>,
     pub transaction_status_meta: status_meta::FuzzTransactionStatusMeta,
     pub index: usize,
 }
@@ -115,4 +172,4 @@ pub struct FuzzTransactionMessage<'a> {
     transaction: FuzzTransaction<'a>,
 }
 
-fuzz_target!(|fuzz_message: FuzzTransactionMessage| {});
+fuzz_target!(|_fuzz_message: FuzzTransactionMessage| {});
