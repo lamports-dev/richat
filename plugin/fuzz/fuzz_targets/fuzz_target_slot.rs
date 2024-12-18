5558bd54a53d0985967a793c2079222f3ed7a9a1
lyranowl <nikita.malina23@gmail.com>
2024-12-18 20:55:21 +0500

    implemented `fuzz_target_slot` fuzz target

--- plugin/fuzz/fuzz_targets/fuzz_target_slot.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_slot.rs
@@ -1,7 +1,44 @@
 #![no_main]
 
+use agave_geyser_plugin_interface::geyser_plugin_interface::SlotStatus;
+use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
+use richat_plugin::protobuf::ProtobufMessage;
 
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+#[derive(Arbitrary, Debug)]
+pub enum FuzzSlotStatus {
+    Processed,
+    Rooted,
+    Confirmed,
+    FirstShredReceived,
+    Completed,
+    CreatedBank,
+    Dead(String),
+}
+
+#[derive(Arbitrary, Debug)]
+pub struct FuzzSlot {
+    slot: u64,
+    parent: Option<u64>,
+    status: FuzzSlotStatus,
+}
+
+fuzz_target!(|fuzz_slot: FuzzSlot| {
+    let mut buf = Vec::new();
+    let status = match fuzz_slot.status {
+        FuzzSlotStatus::Processed => SlotStatus::Processed,
+        FuzzSlotStatus::Rooted => SlotStatus::Rooted,
+        FuzzSlotStatus::Confirmed => SlotStatus::Confirmed,
+        FuzzSlotStatus::FirstShredReceived => SlotStatus::FirstShredReceived,
+        FuzzSlotStatus::Completed => SlotStatus::Completed,
+        FuzzSlotStatus::CreatedBank => SlotStatus::CreatedBank,
+        FuzzSlotStatus::Dead(dead) => SlotStatus::Dead(dead),
+    };
+    let message = ProtobufMessage::Slot {
+        slot: fuzz_slot.slot,
+        parent: fuzz_slot.parent,
+        status: &status,
+    };
+    message.encode(&mut buf);
+    assert!(!buf.is_empty())
 });
