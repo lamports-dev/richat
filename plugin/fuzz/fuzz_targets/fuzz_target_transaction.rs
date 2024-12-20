73e34d4501cefabaf1f900255befad7697ef4a73
lyranowl <nikita.malina23@gmail.com>
2024-12-20 19:03:36 +0500

    implemented `fuzz_target_transaction` fuzz target

--- plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_transaction.rs
@@ -1,4 +1,3 @@
-#![allow(unused)] // FIXME: remove it!!!
 #![no_main]
 
 use {
@@ -6,25 +5,44 @@ use {
     arbitrary::Arbitrary,
     libfuzzer_sys::fuzz_target,
     richat_plugin::protobuf::ProtobufMessage,
-    sanitized::FuzzSanitizedMessage,
-    solana_account_decoder::parse_token::UiTokenAmount,
     solana_sdk::{
         hash::Hash,
         instruction::CompiledInstruction,
-        message::{v0::LoadedAddresses, SimpleAddressLoader, VersionedMessage},
+        message::{v0::LoadedAddresses, SimpleAddressLoader},
         pubkey::Pubkey,
         signature::Signature,
-        signer::Signer,
         signers::Signers,
         transaction::{SanitizedTransaction, SanitizedVersionedTransaction, VersionedTransaction},
-        transaction_context::TransactionReturnData,
     },
-    solana_transaction_status::{
-        InnerInstruction, InnerInstructions, Reward, TransactionStatusMeta, TransactionTokenBalance,
+    solana_transaction_status::TransactionStatusMeta,
+    status_meta::{
+        FuzzInnerInstructions, FuzzReward, FuzzTransactionReturnData, FuzzTransactionTokenBalance,
     },
-    status_meta::FuzzRewardType,
     std::collections::HashSet,
 };
+
+pub struct FuzzSigner;
+
+impl Signers for FuzzSigner {
+    fn pubkeys(&self) -> Vec<Pubkey> {
+        vec![Pubkey::new_unique()]
+    }
+    fn try_pubkeys(&self) -> Result<Vec<Pubkey>, solana_sdk::signer::SignerError> {
+        Ok(vec![Pubkey::new_unique()])
+    }
+    fn sign_message(&self, _message: &[u8]) -> Vec<Signature> {
+        vec![Signature::new_unique()]
+    }
+    fn try_sign_message(
+        &self,
+        _message: &[u8],
+    ) -> Result<Vec<Signature>, solana_sdk::signer::SignerError> {
+        Ok(vec![Signature::new_unique()])
+    }
+    fn is_interactive(&self) -> bool {
+        false
+    }
+}
 
 #[derive(Arbitrary, Debug, Clone)]
 pub struct FuzzCompiledInstruction {
@@ -33,14 +51,54 @@ pub struct FuzzCompiledInstruction {
     data: Vec<u8>,
 }
 
+impl FuzzCompiledInstruction {
+    pub fn into_solana(self) -> CompiledInstruction {
+        CompiledInstruction {
+            program_id_index: self.program_id_index,
+            accounts: self.accounts,
+            data: self.data,
+        }
+    }
+}
+
 #[derive(Arbitrary, Debug, Clone)]
 pub struct FuzzLoadedAddresses {
     writable: Vec<[u8; 32]>,
     readonly: Vec<[u8; 32]>,
 }
 
+impl FuzzLoadedAddresses {
+    pub fn into_solana(self) -> LoadedAddresses {
+        LoadedAddresses {
+            writable: self
+                .writable
+                .into_iter()
+                .map(Pubkey::new_from_array)
+                .collect(),
+            readonly: self
+                .readonly
+                .into_iter()
+                .map(Pubkey::new_from_array)
+                .collect(),
+        }
+    }
+}
+
 pub mod sanitized {
-    use {arbitrary::Arbitrary, std::borrow::Cow};
+    use {
+        crate::FuzzCompiledInstruction,
+        arbitrary::Arbitrary,
+        solana_sdk::{
+            hash::Hash,
+            message::{
+                legacy,
+                v0::{self, MessageAddressTableLookup},
+                LegacyMessage, MessageHeader, VersionedMessage,
+            },
+            pubkey::Pubkey,
+        },
+        std::borrow::Cow,
+    };
 
     #[derive(Arbitrary, Debug, Clone)]
     pub struct FuzzLegacyMessageInner {
@@ -50,6 +108,29 @@ pub mod sanitized {
         pub instructions: Vec<super::FuzzCompiledInstruction>,
     }
 
+    impl FuzzLegacyMessageInner {
+        pub fn into_solana(self) -> legacy::Message {
+            let header = self.header.into_solana();
+            let account_keys = self
+                .account_keys
+                .into_iter()
+                .map(Pubkey::new_from_array)
+                .collect();
+            let recent_blockhash = Hash::new_from_array(self.recent_blockhash);
+            let instructions = self
+                .instructions
+                .into_iter()
+                .map(FuzzCompiledInstruction::into_solana)
+                .collect();
+            legacy::Message {
+                header,
+                account_keys,
+                recent_blockhash,
+                instructions,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug, Clone)]
     pub struct FuzzMessageAddressTableLookup {
         pub account_key: [u8; 32],
@@ -57,11 +138,31 @@ pub mod sanitized {
         pub readonly_indexes: Vec<u8>,
     }
 
+    impl FuzzMessageAddressTableLookup {
+        pub fn into_solana(self) -> MessageAddressTableLookup {
+            MessageAddressTableLookup {
+                account_key: Pubkey::new_from_array(self.account_key),
+                writable_indexes: self.writable_indexes,
+                readonly_indexes: self.readonly_indexes,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug, Clone)]
     pub struct FuzzMessageHeader {
         pub num_required_signatures: u8,
         pub num_readonly_signed_accounts: u8,
         pub num_readonly_unsigned_accounts: u8,
+    }
+
+    impl FuzzMessageHeader {
+        pub fn into_solana(self) -> MessageHeader {
+            MessageHeader {
+                num_required_signatures: self.num_required_signatures,
+                num_readonly_signed_accounts: self.num_readonly_signed_accounts,
+                num_readonly_unsigned_accounts: self.num_readonly_unsigned_accounts,
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug, Clone)]
@@ -73,10 +174,48 @@ pub mod sanitized {
         pub address_table_lookups: Vec<FuzzMessageAddressTableLookup>,
     }
 
+    impl FuzzLoadedMessageInner {
+        pub fn into_solana(self) -> v0::Message {
+            let header = self.header.into_solana();
+            let account_keys = self
+                .account_keys
+                .into_iter()
+                .map(Pubkey::new_from_array)
+                .collect();
+            let recent_blockhash = Hash::new_from_array(self.recent_blockhash);
+            let instructions = self
+                .instructions
+                .into_iter()
+                .map(FuzzCompiledInstruction::into_solana)
+                .collect();
+            let address_table_lookups = self
+                .address_table_lookups
+                .into_iter()
+                .map(FuzzMessageAddressTableLookup::into_solana)
+                .collect();
+            v0::Message {
+                header,
+                account_keys,
+                recent_blockhash,
+                instructions,
+                address_table_lookups,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug)]
     pub struct FuzzLegacyMessage<'a> {
         pub message: Cow<'a, FuzzLegacyMessageInner>,
         pub is_writable_account_cache: Vec<bool>,
+    }
+
+    impl<'a> FuzzLegacyMessage<'a> {
+        pub fn into_solana(self) -> LegacyMessage<'static> {
+            LegacyMessage {
+                message: Cow::Owned(self.message.into_owned().into_solana()),
+                is_writable_account_cache: self.is_writable_account_cache,
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug)]
@@ -86,10 +225,31 @@ pub mod sanitized {
         pub is_writable_account_cache: Vec<bool>,
     }
 
+    impl<'a> FuzzLoadedMessage<'a> {
+        pub fn into_solana(self) -> v0::LoadedMessage<'static> {
+            v0::LoadedMessage {
+                message: Cow::Owned(self.message.into_owned().into_solana()),
+                loaded_addresses: Cow::Owned(self.loaded_addresses.into_owned().into_solana()),
+                is_writable_account_cache: self.is_writable_account_cache,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug)]
     pub enum FuzzSanitizedMessage<'a> {
         Legacy(FuzzLegacyMessage<'a>),
         V0(FuzzLoadedMessage<'a>),
+    }
+
+    impl<'a> FuzzSanitizedMessage<'a> {
+        pub fn into_solana(self) -> VersionedMessage {
+            match self {
+                Self::Legacy(legacy) => {
+                    VersionedMessage::Legacy(legacy.message.into_owned().into_solana())
+                }
+                Self::V0(v0) => VersionedMessage::V0(v0.message.into_owned().into_solana()),
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug)]
@@ -102,7 +262,14 @@ pub mod sanitized {
 }
 
 pub mod status_meta {
-    use {arbitrary::Arbitrary, solana_transaction_status::RewardType};
+    use {
+        arbitrary::Arbitrary,
+        solana_account_decoder::parse_token::UiTokenAmount,
+        solana_sdk::{pubkey::Pubkey, transaction_context::TransactionReturnData},
+        solana_transaction_status::{
+            InnerInstruction, InnerInstructions, Reward, RewardType, TransactionTokenBalance,
+        },
+    };
 
     #[derive(Arbitrary, Debug)]
     pub enum FuzzTransactionError {
@@ -115,10 +282,34 @@ pub mod status_meta {
         pub stack_height: Option<u32>,
     }
 
+    impl FuzzInnerInstruction {
+        pub fn into_solana(self) -> InnerInstruction {
+            let instruction = self.instruction.into_solana();
+            InnerInstruction {
+                instruction,
+                stack_height: self.stack_height,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug)]
     pub struct FuzzInnerInstructions {
         pub index: u8,
         pub instructions: Vec<FuzzInnerInstruction>,
+    }
+
+    impl FuzzInnerInstructions {
+        pub fn into_solana(self) -> InnerInstructions {
+            let instructions = self
+                .instructions
+                .into_iter()
+                .map(FuzzInnerInstruction::into_solana)
+                .collect();
+            InnerInstructions {
+                index: self.index,
+                instructions,
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug)]
@@ -129,6 +320,17 @@ pub mod status_meta {
         pub ui_amount_string: String,
     }
 
+    impl FuzzUiTokenAmount {
+        pub fn into_solana(self) -> UiTokenAmount {
+            UiTokenAmount {
+                ui_amount: self.ui_amount,
+                amount: self.amount,
+                decimals: self.decimals,
+                ui_amount_string: self.ui_amount_string,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug)]
     pub struct FuzzTransactionTokenBalance {
         pub account_index: u8,
@@ -136,6 +338,18 @@ pub mod status_meta {
         pub ui_token_amount: FuzzUiTokenAmount,
         pub owner: String,
         pub program_id: String,
+    }
+
+    impl FuzzTransactionTokenBalance {
+        pub fn into_solana(self) -> TransactionTokenBalance {
+            TransactionTokenBalance {
+                account_index: self.account_index,
+                mint: self.mint,
+                ui_token_amount: self.ui_token_amount.into_solana(),
+                owner: self.owner,
+                program_id: self.program_id,
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug)]
@@ -166,10 +380,31 @@ pub mod status_meta {
         pub commission: Option<u8>,
     }
 
+    impl FuzzReward {
+        pub fn into_solana(self) -> Reward {
+            Reward {
+                pubkey: self.pubkey,
+                lamports: self.lamports,
+                post_balance: self.post_balance,
+                reward_type: self.reward_type.map(FuzzRewardType::into_solana),
+                commission: self.commission,
+            }
+        }
+    }
+
     #[derive(Arbitrary, Debug)]
     pub struct FuzzTransactionReturnData {
         pub program_id: [u8; 32],
         pub data: Vec<u8>,
+    }
+
+    impl FuzzTransactionReturnData {
+        pub fn into_solana(self) -> TransactionReturnData {
+            TransactionReturnData {
+                program_id: Pubkey::new_from_array(self.program_id),
+                data: self.data,
+            }
+        }
     }
 
     #[derive(Arbitrary, Debug)]
@@ -204,35 +439,9 @@ pub struct FuzzTransactionMessage<'a> {
     pub transaction: FuzzTransaction<'a>,
 }
 
-pub struct FuzzSigner;
-
-impl Signers for FuzzSigner {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        vec![Pubkey::new_unique()]
-    }
-    fn try_pubkeys(&self) -> Result<Vec<Pubkey>, solana_sdk::signer::SignerError> {
-        Ok(vec![Pubkey::new_unique()])
-    }
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        vec![Signature::new_unique()]
-    }
-    fn try_sign_message(
-        &self,
-        message: &[u8],
-    ) -> Result<Vec<Signature>, solana_sdk::signer::SignerError> {
-        Ok(vec![Signature::new_unique()])
-    }
-    fn is_interactive(&self) -> bool {
-        false
-    }
-}
-
 fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
     let mut buf = Vec::new();
-    let versioned_message = match fuzz_message.transaction.transaction.message {
-        FuzzSanitizedMessage::Legacy(legacy) => todo!(),
-        FuzzSanitizedMessage::V0(v0) => todo!(),
-    };
+    let versioned_message = fuzz_message.transaction.transaction.message.into_solana();
     let versioned_transaction = VersionedTransaction::try_new(versioned_message, &FuzzSigner)
         .expect("failed to define `VersionedTransaction`");
     let sanitized_versioned_transaction =
@@ -246,7 +455,7 @@ fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
         &HashSet::new(),
     )
     .expect("failed to define `SanitizedTransaction`");
-    let status = ();
+    let _status = ();
     let inner_instructions = fuzz_message
         .transaction
         .transaction_status_meta
@@ -254,28 +463,8 @@ fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
         .map(|inner_instructions| {
             inner_instructions
                 .into_iter()
-                .map(|inner_instructions| {
-                    let instructions = inner_instructions
-                        .instructions
-                        .into_iter()
-                        .map(|inner_instruction| {
-                            let instruction = CompiledInstruction {
-                                program_id_index: inner_instruction.instruction.program_id_index,
-                                accounts: inner_instruction.instruction.accounts,
-                                data: inner_instruction.instruction.data,
-                            };
-                            InnerInstruction {
-                                instruction,
-                                stack_height: inner_instruction.stack_height,
-                            }
-                        })
-                        .collect();
-                    InnerInstructions {
-                        index: inner_instructions.index,
-                        instructions,
-                    }
-                })
-                .collect::<Vec<_>>()
+                .map(FuzzInnerInstructions::into_solana)
+                .collect()
         });
     let pre_token_balances = fuzz_message
         .transaction
@@ -284,22 +473,8 @@ fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
         .map(|pre_token_balances| {
             pre_token_balances
                 .into_iter()
-                .map(|token_balance| {
-                    let ui_token_amount = UiTokenAmount {
-                        ui_amount: token_balance.ui_token_amount.ui_amount,
-                        amount: token_balance.ui_token_amount.amount,
-                        decimals: token_balance.ui_token_amount.decimals,
-                        ui_amount_string: token_balance.ui_token_amount.ui_amount_string,
-                    };
-                    TransactionTokenBalance {
-                        account_index: token_balance.account_index,
-                        mint: token_balance.mint,
-                        ui_token_amount,
-                        owner: token_balance.owner,
-                        program_id: token_balance.program_id,
-                    }
-                })
-                .collect::<Vec<_>>()
+                .map(FuzzTransactionTokenBalance::into_solana)
+                .collect()
         });
     let post_token_balances = fuzz_message
         .transaction
@@ -308,22 +483,8 @@ fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
         .map(|post_token_balances| {
             post_token_balances
                 .into_iter()
-                .map(|token_balance| {
-                    let ui_token_amount = UiTokenAmount {
-                        ui_amount: token_balance.ui_token_amount.ui_amount,
-                        amount: token_balance.ui_token_amount.amount,
-                        decimals: token_balance.ui_token_amount.decimals,
-                        ui_amount_string: token_balance.ui_token_amount.ui_amount_string,
-                    };
-                    TransactionTokenBalance {
-                        account_index: token_balance.account_index,
-                        mint: token_balance.mint,
-                        ui_token_amount,
-                        owner: token_balance.owner,
-                        program_id: token_balance.program_id,
-                    }
-                })
-                .collect::<Vec<_>>()
+                .map(FuzzTransactionTokenBalance::into_solana)
+                .collect()
         });
     let rewards = fuzz_message
         .transaction
@@ -332,41 +493,19 @@ fuzz_target!(|fuzz_message: FuzzTransactionMessage| {
         .map(|rewards| {
             rewards
                 .into_iter()
-                .map(|reward| Reward {
-                    pubkey: reward.pubkey,
-                    lamports: reward.lamports,
-                    post_balance: reward.post_balance,
-                    reward_type: reward.reward_type.map(FuzzRewardType::into_solana),
-                    commission: reward.commission,
-                })
+                .map(FuzzReward::into_solana)
                 .collect::<Vec<_>>()
         });
-    let loaded_addresses = LoadedAddresses {
-        writable: fuzz_message
-            .transaction
-            .transaction_status_meta
-            .loaded_addresses
-            .writable
-            .into_iter()
-            .map(Pubkey::new_from_array)
-            .collect(),
-        readonly: fuzz_message
-            .transaction
-            .transaction_status_meta
-            .loaded_addresses
-            .readonly
-            .into_iter()
-            .map(Pubkey::new_from_array)
-            .collect(),
-    };
+    let loaded_addresses = fuzz_message
+        .transaction
+        .transaction_status_meta
+        .loaded_addresses
+        .into_solana();
     let return_data = fuzz_message
         .transaction
         .transaction_status_meta
         .return_data
-        .map(|return_data| TransactionReturnData {
-            program_id: Pubkey::new_from_array(return_data.program_id),
-            data: return_data.data,
-        });
+        .map(FuzzTransactionReturnData::into_solana);
     let transaction_status_meta = TransactionStatusMeta {
         status: Ok(()), // TODO
         fee: fuzz_message.transaction.transaction_status_meta.fee,
