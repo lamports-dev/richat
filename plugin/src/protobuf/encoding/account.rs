2d404280b4739731b78d99d9b54f72a0ac94b260
lyranowl <nikita.malina23@gmail.com>
2024-12-23 15:35:59 +0500

    fix: encoding `Account`

--- plugin/src/protobuf/encoding/account.rs
+++ plugin/src/protobuf/encoding/account.rs
@@ -17,22 +17,36 @@ impl<'a> prost::Message for Account<'a> {
         encode_varint(self.account_encoded_len() as u64, buf);
 
         bytes_encode(1, self.account.pubkey, buf);
-        encoding::uint64::encode(2, &self.account.lamports, buf);
+        if self.account.lamports != 0 {
+            encoding::uint64::encode(2, &self.account.lamports, buf);
+        };
         bytes_encode(3, self.account.owner, buf);
-        encoding::bool::encode(4, &self.account.executable, buf);
-        encoding::uint64::encode(5, &self.account.rent_epoch, buf);
+        if self.account.executable {
+            encoding::bool::encode(4, &self.account.executable, buf);
+        }
+        if self.account.rent_epoch != 0 {
+            encoding::uint64::encode(5, &self.account.rent_epoch, buf);
+        }
         bytes_encode(6, self.account.data, buf);
-        encoding::uint64::encode(7, &self.account.write_version, buf);
+        if self.account.write_version != 0 {
+            encoding::uint64::encode(7, &self.account.write_version, buf);
+        }
         if let Some(txn) = self.account.txn {
             bytes_encode(8, txn.signature().as_ref(), buf);
         }
 
-        encoding::uint64::encode(2, &self.slot, buf)
+        if self.slot != 0 {
+            encoding::uint64::encode(2, &self.slot, buf)
+        }
     }
 
     fn encoded_len(&self) -> usize {
         field_encoded_len(1, self.account_encoded_len())
-            + encoding::uint64::encoded_len(2, &self.slot)
+            + if self.slot != 0 {
+                encoding::uint64::encoded_len(2, &self.slot)
+            } else {
+                0
+            }
     }
 
     fn merge_field(
@@ -60,12 +74,28 @@ impl<'a> Account<'a> {
 
     fn account_encoded_len(&self) -> usize {
         bytes_encoded_len(1u32, self.account.pubkey)
-            + encoding::uint64::encoded_len(2u32, &self.account.lamports)
+            + if self.account.lamports != 0 {
+                encoding::uint64::encoded_len(2u32, &self.account.lamports)
+            } else {
+                0
+            }
             + bytes_encoded_len(3u32, self.account.owner)
-            + encoding::bool::encoded_len(4u32, &self.account.executable)
-            + encoding::uint64::encoded_len(5u32, &self.account.rent_epoch)
+            + if self.account.executable {
+                encoding::bool::encoded_len(4u32, &self.account.executable)
+            } else {
+                0
+            }
+            + if self.account.rent_epoch != 0 {
+                encoding::uint64::encoded_len(5u32, &self.account.rent_epoch)
+            } else {
+                0
+            }
             + bytes_encoded_len(6u32, self.account.data)
-            + encoding::uint64::encoded_len(7u32, &self.account.write_version)
+            + if self.account.write_version != 0 {
+                encoding::uint64::encoded_len(7u32, &self.account.write_version)
+            } else {
+                0
+            }
             + self
                 .account
                 .txn
