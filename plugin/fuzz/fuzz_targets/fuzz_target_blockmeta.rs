45becb78c65fce9c6fc344bc855190c7fa56a891
lyranowl <nikita.malina23@gmail.com>
2024-12-23 16:49:39 +0500

    refactor: `fuzz_target_blockmeta` fuzz target

--- plugin/fuzz/fuzz_targets/fuzz_target_blockmeta.rs
+++ plugin/fuzz/fuzz_targets/fuzz_target_blockmeta.rs
@@ -6,12 +6,10 @@ use {
     libfuzzer_sys::fuzz_target,
     prost::{Enumeration, Message},
     richat_plugin::protobuf::ProtobufMessage,
-    solana_transaction_status::RewardsAndNumPartitions,
+    solana_transaction_status::{RewardType, RewardsAndNumPartitions},
 };
 
-// TODO: refactor
-
-#[derive(Message, PartialEq)]
+#[derive(Clone, Message, PartialEq)]
 pub struct Reward {
     #[prost(string, tag = "1")]
     pub pubkey: String,
@@ -19,22 +17,13 @@ pub struct Reward {
     pub lamports: i64,
     #[prost(uint64, tag = "3")]
     pub post_balance: u64,
-    #[prost(enumeration = "RewardType", tag = "4")]
+    #[prost(enumeration = "FuzzRewardType", tag = "4")]
     pub reward_type: i32,
     #[prost(string, tag = "5")]
     pub commission: String,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Enumeration)]
-#[repr(i32)]
-pub enum RewardType {
-    Fee = 1,
-    Rent = 2,
-    Staking = 3,
-    Voting = 4,
-}
-
-#[derive(Message, PartialEq)]
+#[derive(Clone, Message, PartialEq)]
 pub struct Rewards {
     #[prost(message, repeated, tag = "1")]
     rewards: Vec<Reward>,
@@ -64,7 +53,7 @@ pub struct BlockMeta {
     entry_count: u64,
 }
 
-#[derive(Clone, Copy, Arbitrary, Debug)]
+#[derive(Clone, Copy, Arbitrary, Debug, Enumeration)]
 #[repr(i32)]
 pub enum FuzzRewardType {
     Fee = 1,
@@ -73,13 +62,13 @@ pub enum FuzzRewardType {
     Voting = 4,
 }
 
-impl Into<solana_transaction_status::RewardType> for FuzzRewardType {
-    fn into(self) -> solana_transaction_status::RewardType {
+impl Into<RewardType> for FuzzRewardType {
+    fn into(self) -> RewardType {
         match self {
-            FuzzRewardType::Fee => solana_transaction_status::RewardType::Fee,
-            FuzzRewardType::Rent => solana_transaction_status::RewardType::Rent,
-            FuzzRewardType::Staking => solana_transaction_status::RewardType::Staking,
-            FuzzRewardType::Voting => solana_transaction_status::RewardType::Voting,
+            FuzzRewardType::Fee => RewardType::Fee,
+            FuzzRewardType::Rent => RewardType::Rent,
+            FuzzRewardType::Staking => RewardType::Staking,
+            FuzzRewardType::Voting => RewardType::Voting,
         }
     }
 }
@@ -123,53 +112,54 @@ fuzz_target!(|fuzz_blockmeta: FuzzBlockMeta| {
             .collect(),
         num_partitions: fuzz_blockmeta.num_partitions,
     };
+    let blockinfo = ReplicaBlockInfoV4 {
+        parent_slot: fuzz_blockmeta.parent_slot,
+        parent_blockhash: fuzz_blockmeta.parent_blockhash,
+        slot: fuzz_blockmeta.slot,
+        blockhash: fuzz_blockmeta.blockhash,
+        rewards: &rewards_and_num_partitions,
+        block_time: fuzz_blockmeta.block_time,
+        block_height: fuzz_blockmeta.block_height,
+        executed_transaction_count: fuzz_blockmeta.executed_transaction_count,
+        entry_count: fuzz_blockmeta.entry_count,
+    };
     let message = ProtobufMessage::BlockMeta {
-        blockinfo: &ReplicaBlockInfoV4 {
-            parent_slot: fuzz_blockmeta.parent_slot,
-            parent_blockhash: fuzz_blockmeta.parent_blockhash,
-            slot: fuzz_blockmeta.slot,
-            blockhash: fuzz_blockmeta.blockhash,
-            rewards: &rewards_and_num_partitions,
-            block_time: fuzz_blockmeta.block_time,
-            block_height: fuzz_blockmeta.block_height,
-            executed_transaction_count: fuzz_blockmeta.executed_transaction_count,
-            entry_count: fuzz_blockmeta.entry_count,
-        },
+        blockinfo: &blockinfo,
     };
     message.encode(&mut buf);
     assert!(!buf.is_empty());
+
     let decoded = BlockMeta::decode(buf.as_slice()).expect("failed to decode `BlockMeta` from buf");
     assert_eq!(decoded.parent_slot, fuzz_blockmeta.parent_slot);
     assert_eq!(&decoded.parent_blockhash, fuzz_blockmeta.parent_blockhash);
     assert_eq!(decoded.slot, fuzz_blockmeta.slot);
     assert_eq!(&decoded.blockhash, fuzz_blockmeta.blockhash);
-    // TODO: refactor
+    let from_blockmeta_rewards = fuzz_blockmeta
+        .rewards
+        .iter()
+        .map(|reward| Reward {
+            pubkey: reward.pubkey.to_owned(),
+            lamports: reward.lamports,
+            post_balance: reward.post_balance,
+            reward_type: reward
+                .reward_type
+                .as_ref()
+                .map_or(0, |reward_type| *reward_type as i32),
+            commission: reward
+                .commission
+                .map_or(0u8.to_string(), |commission| commission.to_string()),
+        })
+        .collect::<Vec<_>>();
     assert_eq!(
-        decoded.rewards.as_ref().map(|rewards| &rewards.rewards),
-        Some(
-            &fuzz_blockmeta
-                .rewards
-                .iter()
-                .map(|reward| {
-                    Reward {
-                        pubkey: reward.pubkey.to_owned(),
-                        lamports: reward.lamports,
-                        post_balance: reward.post_balance,
-                        reward_type: reward
-                            .reward_type
-                            .as_ref()
-                            .map_or(0, |reward_type| *reward_type as i32),
-                        commission: reward
-                            .commission
-                            .map_or(0u8.to_string(), |commission| commission.to_string()),
-                    }
-                })
-                .collect()
-        )
+        decoded
+            .rewards
+            .as_ref()
+            .map_or(Vec::default(), |rewards| rewards.rewards.to_owned()),
+        from_blockmeta_rewards
     );
     assert_eq!(
-        decoded.rewards.map(|reward| reward.num_partitions),
-        Some(fuzz_blockmeta.num_partitions)
+        decoded.rewards.map_or(0, |reward| reward.num_partitions()),
+        fuzz_blockmeta.num_partitions.unwrap_or_default()
     );
     assert_eq!(decoded.block_time, fuzz_blockmeta.block_time);
     assert_eq!(decoded.block_height, fuzz_blockmeta.block_height);
