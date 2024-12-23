d7fd3a5c492cff483f235bee9484e2b05f93d34b
Kirill Fomichev <fanatid@ya.ru>
2024-12-22 22:06:23 -0500

    setup logs

--- richat/src/bin/richat.rs
+++ richat/src/bin/richat.rs
@@ -5,13 +5,16 @@ use {anyhow::Context, clap::Parser, richat::config::Config};
 struct Args {
     #[clap(short, long, default_value_t = String::from("config.json"))]
     /// Path to config
-    config: String,
+    pub config: String,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let config = Config::load_from_file(&args.config)
         .with_context(|| format!("failed to load config from {}", args.config))?;
+
+    // Setup logs
+    richat::log::setup(config.log.json)?;
 
     Ok(())
 }
--- richat/src/config.rs
+++ richat/src/config.rs
@@ -3,18 +3,28 @@ use {
     std::{fs, path::Path},
 };
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Deserialize)]
+#[serde(deny_unknown_fields, default)]
 pub struct Config {
-    //
+    pub log: ConfigLog,
 }
 
 impl Config {
-    fn load_from_str(config: &str) -> anyhow::Result<Self> {
-        serde_json::from_str(config).map_err(Into::into)
-    }
-
     pub fn load_from_file<P: AsRef<Path>>(file: P) -> anyhow::Result<Self> {
-        let config = fs::read_to_string(file)?;
-        Self::load_from_str(&config)
+        let config = fs::read_to_string(&file)?;
+        if matches!(
+            file.as_ref().extension().and_then(|e| e.to_str()),
+            Some("yml") | Some("yaml")
+        ) {
+            serde_yaml::from_str(&config).map_err(Into::into)
+        } else {
+            json5::from_str(&config).map_err(Into::into)
+        }
     }
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(deny_unknown_fields, default)]
+pub struct ConfigLog {
+    pub json: bool,
 }
