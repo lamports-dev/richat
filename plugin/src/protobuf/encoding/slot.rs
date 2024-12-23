d61de242e82d11e16031cc9d385316dc916b1e9e
lyranowl <nikita.malina23@gmail.com>
2024-12-23 15:42:54 +0500

    fix: encoding `Slot`

--- plugin/src/protobuf/encoding/slot.rs
+++ plugin/src/protobuf/encoding/slot.rs
@@ -1,5 +1,25 @@
 use {agave_geyser_plugin_interface::geyser_plugin_interface::SlotStatus, prost::encoding};
 
+const fn slot_status_as_i32(status: &SlotStatus) -> i32 {
+    match status {
+        SlotStatus::Processed => 0,
+        SlotStatus::Rooted => 1,
+        SlotStatus::Confirmed => 2,
+        SlotStatus::FirstShredReceived => 3,
+        SlotStatus::Completed => 4,
+        SlotStatus::CreatedBank => 5,
+        SlotStatus::Dead(_) => 6,
+    }
+}
+
+const fn is_slot_status_dead(status: &SlotStatus) -> Option<&String> {
+    if let SlotStatus::Dead(dead) = status {
+        Some(dead)
+    } else {
+        None
+    }
+}
+
 #[derive(Debug)]
 pub struct Slot<'a> {
     slot: solana_sdk::clock::Slot,
@@ -11,11 +31,15 @@ impl<'a> prost::Message for Slot<'a> {
     fn encode_raw(&self, buf: &mut impl prost::bytes::BufMut) {
         let status = slot_status_as_i32(self.status);
         let dead = is_slot_status_dead(self.status);
-        encoding::uint64::encode(1, &self.slot, buf);
+        if self.slot != 0 {
+            encoding::uint64::encode(1, &self.slot, buf)
+        }
         if let Some(ref value) = self.parent {
             encoding::uint64::encode(2, value, buf)
         }
-        encoding::int32::encode(3, &status, buf);
+        if status != slot_status_as_i32(&SlotStatus::Processed) {
+            encoding::int32::encode(3, &status, buf)
+        }
         if let Some(value) = dead {
             encoding::string::encode(4, value, buf)
         }
@@ -24,17 +48,20 @@ impl<'a> prost::Message for Slot<'a> {
     fn encoded_len(&self) -> usize {
         let status = slot_status_as_i32(self.status);
         let dead = is_slot_status_dead(self.status);
-        encoding::uint64::encoded_len(1, &self.slot)
-            + self
-                .parent
-                .as_ref()
-                .map_or(0, |value| encoding::uint64::encoded_len(2, value))
-            + encoding::int32::encoded_len(3, &status)
-            + if let Some(value) = dead {
-                encoding::string::encoded_len(4, value)
+        (if self.slot != 0 {
+            encoding::uint64::encoded_len(1, &self.slot)
+        } else {
+            0
+        }) + self
+            .parent
+            .as_ref()
+            .map_or(0, |value| encoding::uint64::encoded_len(2, value))
+            + if status != slot_status_as_i32(&SlotStatus::Processed) {
+                encoding::int32::encoded_len(3, &status)
             } else {
                 0
             }
+            + dead.map_or(0, |dead| encoding::string::encoded_len(4, dead))
     }
 
     fn merge_field(
@@ -66,25 +93,5 @@ impl<'a> Slot<'a> {
             parent,
             status,
         }
-    }
-}
-
-const fn slot_status_as_i32(status: &SlotStatus) -> i32 {
-    match status {
-        SlotStatus::Processed => 0,
-        SlotStatus::Rooted => 1,
-        SlotStatus::Confirmed => 2,
-        SlotStatus::FirstShredReceived => 3,
-        SlotStatus::Completed => 4,
-        SlotStatus::CreatedBank => 5,
-        SlotStatus::Dead(_) => 6,
-    }
-}
-
-const fn is_slot_status_dead(status: &SlotStatus) -> Option<&String> {
-    if let SlotStatus::Dead(dead) = status {
-        Some(dead)
-    } else {
-        None
     }
 }
