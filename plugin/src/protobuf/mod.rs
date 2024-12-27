bc0a388ac5745dff47311275ad4e9593e877fbe5
lyranowl <nikita.malina23@gmail.com>
2024-12-27 22:21:50 +0500

    implemented decoding `Transaction`

--- plugin/src/protobuf/mod.rs
+++ plugin/src/protobuf/mod.rs
@@ -13,7 +13,6 @@ mod tests {
         predefined::load_predefined_blocks,
         prost::{Enumeration, Message},
         solana_sdk::{hash::Hash, pubkey::Pubkey},
-        yellowstone_grpc_proto::prelude::NumPartitions,
     };
 
     mod predefined {
@@ -413,6 +412,187 @@ mod tests {
         assert_eq!(decoded.slot, 0);
         assert_eq!(decoded.parent, Some(1));
         assert_eq!(decoded.status, 0)
+    }
+
+    /*
+    super::message_header::encode_message_header(1, message.header, buf);
+    super::pubkeys::encode_pubkeys(2, &message.account_keys, buf);
+    super::recent_blockhash::encode_recent_blockhash(
+        3,
+        message.recent_blockhash.as_ref(),
+        buf,
+    );
+    super::compiled_instructions::encode_compiled_instructions(
+        4,
+        &message.instructions,
+        buf,
+    );
+    super::versioned::encode_versioned(5, true, buf);
+    super::address_table_lookups::encode_address_table_lookups(
+        6,
+        &message.address_table_lookups,
+        buf,
+    )
+    */
+
+    #[derive(Message)]
+    pub struct MessageAddressTableLookup {
+        #[prost(bytes = "vec", tag = "1")]
+        account_key: Vec<u8>,
+        #[prost(bytes = "vec", tag = "2")]
+        writable_indexes: Vec<u8>,
+        #[prost(bytes = "vec", tag = "3")]
+        readonly_indexes: Vec<u8>,
+    }
+
+    #[derive(Message)]
+    pub struct CompiledInstruction {
+        #[prost(uint32, tag = "1")]
+        program_id_index: u32,
+        #[prost(bytes = "vec", tag = "2")]
+        accounts: Vec<u8>,
+        #[prost(bytes = "vec", tag = "3")]
+        data: Vec<u8>,
+    }
+
+    #[derive(Message)]
+    pub struct MessageHeader {
+        #[prost(uint32, tag = "1")]
+        num_required_signatures: u32,
+        #[prost(uint32, tag = "2")]
+        num_readonly_signed_accounts: u32,
+        #[prost(uint32, tag = "3")]
+        num_readonly_unsigned_accounts: u32,
+    }
+
+    #[derive(Message)]
+    pub struct LoadedMessage {
+        #[prost(message, tag = "1")]
+        header: Option<MessageHeader>,
+        #[prost(bytes = "vec", repeated, tag = "2")]
+        account_keys: Vec<Vec<u8>>,
+        #[prost(bytes = "vec", tag = "3")]
+        recent_blockhash: Vec<u8>,
+        #[prost(message, repeated, tag = "4")]
+        compiled_instructions: Vec<CompiledInstruction>,
+        #[prost(bool, tag = "5")]
+        versioned: bool,
+        #[prost(message, tag = "6")]
+        address_table_lookup: Option<MessageAddressTableLookup>,
+    }
+
+    #[derive(Message)]
+    pub struct SanitizedTransaction {
+        #[prost(message, tag = "1")]
+        message: Option<LoadedMessage>,
+        #[prost(bytes = "vec", repeated, tag = "2")]
+        signatures: Vec<Vec<u8>>,
+    }
+
+    #[derive(Message)]
+    pub struct TransactionReturnData {
+        #[prost(bytes = "vec", tag = "1")]
+        program_id: Vec<u8>,
+        #[prost(bytes = "vec", tag = "2")]
+        data: Vec<u8>,
+    }
+
+    #[derive(Message)]
+    pub struct UiTokenAmount {
+        #[prost(double, optional, tag = "1")]
+        ui_amount: Option<f64>,
+        #[prost(uint32, tag = "2")]
+        decimals: u32,
+        #[prost(string, tag = "3")]
+        amount: String,
+        #[prost(string, tag = "4")]
+        ui_amount_string: String,
+    }
+
+    #[derive(Message)]
+    pub struct TransactionTokenBalance {
+        #[prost(uint32, tag = "1")]
+        account_index: u32,
+        #[prost(string, tag = "2")]
+        mint: String,
+        #[prost(message, tag = "3")]
+        ui_token_amount: Option<UiTokenAmount>,
+        #[prost(string, tag = "4")]
+        owner: String,
+        #[prost(string, tag = "5")]
+        program_id: String,
+    }
+
+    #[derive(Message)]
+    pub struct InnerInstruction {
+        #[prost(message, tag = "1")]
+        instruction: Option<CompiledInstruction>,
+        #[prost(uint32, optional, tag = "2")]
+        stack_height: Option<u32>,
+    }
+
+    #[derive(Message)]
+    pub struct InnerInstructions {
+        #[prost(uint32, tag = "1")]
+        index: u32,
+        #[prost(message, tag = "2")]
+        instruction: Option<InnerInstruction>,
+    }
+
+    #[derive(Message)]
+    pub struct TransactionStatusMeta {
+        #[prost(uint64, tag = "2")]
+        fee: u64,
+        #[prost(uint64, repeated, tag = "3")]
+        pre_balances: Vec<u64>,
+        #[prost(uint64, repeated, tag = "4")]
+        post_balances: Vec<u64>,
+        #[prost(message, repeated, tag = "5")]
+        inner_instructions: Vec<InnerInstructions>,
+        #[prost(string, repeated, tag = "6")]
+        log_messages: Vec<String>,
+        #[prost(message, repeated, tag = "7")]
+        pre_token_balances: Vec<TransactionTokenBalance>,
+        #[prost(message, repeated, tag = "8")]
+        post_token_balances: Vec<TransactionTokenBalance>,
+        #[prost(message, repeated, tag = "9")]
+        rewards: Vec<Reward>,
+        #[prost(bool, tag = "10")]
+        inner_instructions_is_none: bool,
+        #[prost(bool, tag = "11")]
+        log_messages_is_none: bool,
+        #[prost(bytes = "vec", repeated, tag = "12")]
+        loaded_writable_addresses: Vec<Vec<u8>>,
+        #[prost(bytes = "vec", repeated, tag = "13")]
+        loaded_readonly_addresses: Vec<Vec<u8>>,
+        #[prost(message, tag = "14")]
+        return_data: Option<TransactionReturnData>,
+        #[prost(bool, tag = "15")]
+        return_data_is_none: bool,
+        #[prost(uint64, optional, tag = "16")]
+        compute_units_consumed: Option<u64>,
+    }
+
+    #[derive(Message)]
+    pub struct ReplicaTransaction {
+        #[prost(bytes = "vec", tag = 1)]
+        signature: Vec<u8>,
+        #[prost(bool, tag = "2")]
+        is_vote: bool,
+        #[prost(message, tag = "3")]
+        transaction: Option<SanitizedTransaction>,
+        #[prost(message, tag = "4")]
+        transaction_status_meta: Option<TransactionStatusMeta>,
+        #[prost(uint64, tag = "5")]
+        index: u64,
+    }
+
+    #[derive(Message)]
+    pub struct Transaction {
+        #[prost(message, tag = "1")]
+        transaction: Option<ReplicaTransaction>,
+        #[prost(uint64, tag = "2")]
+        slot: u64,
     }
 
     #[test]
